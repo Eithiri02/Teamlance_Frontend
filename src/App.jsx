59da7acf144ee3da4a787573747d0c90e6c81083
Eithiri02 <eithiriaung.devit@gmail.com>
2023-02-16 15:41:37 +0630

    App file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { PrivateRoute,PublicRoute } from '_components';
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
+import { PrivateRoute, PublicRoute } from '_components';
 import { Home } from 'home';
 import { Login } from 'login';
 import { Projects } from './_components/Projects';
+import { history } from '_helpers';
 
 export { App };
 
 function App() {
 
+    history.navigate = useNavigate();
+    history.location = useLocation();
+
     return (
         <Routes>
-                <Route element={<PrivateRoute><Home /></PrivateRoute>} >
-                    <Route path='/project' element={<Projects />}></Route>
-                </Route>
-                <Route path='/' element={<PublicRoute><Login /></PublicRoute>}></Route>
-                <Route path='*' element={<Navigate to="/" />} />
-            </Routes>
+            <Route element={<PrivateRoute><Home /></PrivateRoute>} >
+                <Route path='/project' element={<Projects />}></Route>
+            </Route>
+            <Route path='/' element={<PublicRoute><Login /></PublicRoute>}></Route>
+            <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
     );
 }
